50cf683aa5e2ba2df240a50320491f676a10a5b0
Luke Sherwood <luke.sherwood55@gmail.com>
2020-02-16 04:18:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,10 +17,11 @@ class Student
     end
   end
 
-  def add_student_attributes()
+  def add_student_attributes(attributes_hash)
    attributes_hash.each do |key, value|
     self.send("#{key}=", value)
   end
+end
 
   def self.all
     @@all
